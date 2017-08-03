bd4779ec1fea6c7c1748951952a3cb906d5a72ae
Zain <zain@halai.us>
2017-08-03 17:11:01 -0500

    Created models and migration Added Rakefile

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+
+  has_many :posts
+  has_many :comments
+
+  has_secure_password
+  
+end
---
+++ db/migrate/20170803220518_create_comments.rb
@@ -0,0 +1,13 @@
+class CreateComments < ActiveRecord::Migration[5.1]
+
+  def change
+    create_table :comments do |t|
+      t.text :content
+      t.integer :user_id
+      t.integer :post_id
+
+      t.timestamp
+    end
+  end
+
+end
